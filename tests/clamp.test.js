8251fa72811437eebe11d704f3c7e4c799051447
AceHanded <vili.kononen@gmail.com>
2025-11-14 18:49:13 +0200

    Add unit tests for toString.js (closes #10) and mark failing tests

--- tests/clamp.test.js
+++ tests/clamp.test.js
@@ -3,19 +3,19 @@
 import clamp from '../src/clamp.js';
 
 describe('clamp', () => {
-  test('number is within the range', () => {
-    expect(clamp(-7, -5, -10)).toBe(-7);
+  test.failing('number is within the range', () => {
+    expect(clamp(-7, -10, -5)).toBe(-7);
     expect(clamp(7, 5, 10)).toBe(7);
   });
 
-  test('number is outside the range', () => {
+  test.failing('number is outside the range', () => {
     expect(clamp(-11, -10, -5)).toBe(-10);
     expect(clamp(-1, -10, -5)).toBe(-5);
     expect(clamp(1, 5, 10)).toBe(5);
     expect(clamp(11, 5, 10)).toBe(10);
   });
 
-  test('number is on the edge of the range', () => {
+  test.failing('number is on the edge of the range', () => {
     expect(clamp(-10, -10, -5)).toBe(-10);
     expect(clamp(-5, -10, -5)).toBe(-5);
     expect(clamp(5, 5, 10)).toBe(5);
@@ -31,18 +31,18 @@ describe('clamp', () => {
     expect(clamp(NaN, 5, 10)).toBeNaN();
   });
 
-  test('bounds are NaN', () => {
+  test.failing('bounds are NaN', () => {
     expect(clamp(-5, NaN, 5)).toBe(0);
     expect(clamp(5, -5, NaN)).toBe(0);
   });
 
-  test('number is null', () => {
+  test.failing('number is null', () => {
     expect(clamp(null, -5, 5)).toBe(0);
     expect(clamp(null, 5, 10)).toBe(5);
     expect(clamp(null, 0, 5)).toBe(0);
   });
 
-  test('bounds are null', () => {
+  test.failing('bounds are null', () => {
     expect(clamp(-5, null, 5)).toBe(0);
     expect(clamp(5, -5, null)).toBe(0);
   });
@@ -51,12 +51,12 @@ describe('clamp', () => {
     expect(clamp(undefined, 5, 10)).toBeNaN();
   });
 
-  test('bounds are undefined', () => {
+  test.failing('bounds are undefined', () => {
     expect(clamp(-5, undefined, 5)).toBe(0);
     expect(clamp(5, -5, undefined)).toBe(0);
   });
 
-  test('number is float', () => {
+  test.failing('number is float', () => {
     expect(clamp(-0.5, -5, 0)).toBe(-0.5);
     expect(clamp(-0.5, -10, -5)).toBe(-5);
     expect(clamp(-0.5, -5, -0.5)).toBe(-0.5);
@@ -65,14 +65,14 @@ describe('clamp', () => {
     expect(clamp(0.5, 0.5, 5)).toBe(0.5);
   });
 
-  test('bounds are floats', () => {
+  test.failing('bounds are floats', () => {
     expect(clamp(-5, -0.5, 5)).toBe(-0.5);
     expect(clamp(5, -5, -0.5)).toBe(-0.5);
     expect(clamp(-5, 0.5, 5)).toBe(0.5);
     expect(clamp(5, -5, 0.5)).toBe(0.5);
   });
 
-  test('number is boolean', () => {
+  test.failing('number is boolean', () => {
     expect(clamp(false, -5, 5)).toBe(0);
     expect(clamp(false, 5, 10)).toBe(5);
     expect(clamp(false, 0, 5)).toBe(0);
@@ -81,24 +81,24 @@ describe('clamp', () => {
     expect(clamp(true, 1, 5)).toBe(1);
   });
 
-  test('bounds are booleans', () => {
+  test.failing('bounds are booleans', () => {
     expect(clamp(-5, false, 5)).toBe(0);
     expect(clamp(5, -5, false)).toBe(0);
     expect(clamp(-5, true, 5)).toBe(1);
     expect(clamp(5, -5, true)).toBe(1);
   });
 
-  test('number is infinity', () => {
+  test.failing('number is infinity', () => {
     expect(clamp(-Infinity, 5, 10)).toBe(5);
     expect(clamp(Infinity, 5, 10)).toBe(10);
   });
 
-  test('bounds are infinity', () => {
+  test.failing('bounds are infinity', () => {
     expect(clamp(-5, -Infinity, 5)).toBe(-5);
     expect(clamp(5, -5, Infinity)).toBe(5);
   });
 
-  test('number is string', () => {
+  test.failing('number is string', () => {
     expect(clamp('', -5, 5)).toBe(0);
     expect(clamp('', 5, 10)).toBe(5);
     expect(clamp('', 0, 5)).toBe(0);
@@ -108,7 +108,7 @@ describe('clamp', () => {
     expect(clamp('abc', 5, 10)).toBeNaN();
   });
 
-  test('bounds are strings', () => {
+  test.failing('bounds are strings', () => {
     expect(clamp(-5, '', 5)).toBe(0);
     expect(clamp(5, -5, '')).toBe(0);
     expect(clamp(-5, '1', 5)).toBe(1);
--- tests/toString.test.js
+++ tests/toString.test.js
@@ -2,6 +2,73 @@
 
 import toString from '../src/toString.js';
 
-test('placeholder', () => {
-  expect(true).toBe(true);
+describe('toString', () => {
+  test('value is a string', () => {
+    expect(toString('')).toBe('');
+    expect(toString('123')).toBe('123');
+    expect(toString('abc')).toBe('abc');
+  });
+
+  test('value is a symbol', () => {
+    const symbol = Symbol('x'), symbolObject = Object(Symbol('x'));
+
+    expect(toString(symbol)).toBe(symbol.toString());
+    expect(toString(symbolObject)).toBe(symbolObject.toString());
+  });
+
+  test('value is an integer', () => {
+    expect(toString(0)).toBe('0');
+    expect(toString(-0)).toBe('-0');
+    expect(toString(123)).toBe('123');
+    expect(toString(-123)).toBe('-123');
+  });
+  
+  test('value is NaN', () => {
+    expect(toString(NaN)).toBe('NaN');
+  });
+
+  test('value is null', () => {
+    expect(toString(null)).toBe('null');
+  });
+
+  test('value is undefined', () => {
+    expect(toString(undefined)).toBe('undefined');
+  });
+
+  test.failing('value is float', () => {
+    expect(toString(0.0)).toBe('0.0');
+    expect(toString(-0.0)).toBe('-0.0');
+    expect(toString(0.5)).toBe('0.5');
+    expect(toString(-0.5)).toBe('-0.5');
+  });
+
+  test('value is boolean', () => {
+    expect(toString(false)).toBe('false');
+    expect(toString(true)).toBe('true');
+  });
+
+  test('value is infinity', () => {
+    expect(toString(Infinity)).toBe('Infinity');
+    expect(toString(-Infinity)).toBe('-Infinity');
+  });
+
+  test.failing('value is an array', () => {
+    const symbol = Symbol('x'), symbol2 = Symbol('y'), symbol3 = Symbol('z');
+    
+    expect(toString([])).toBe('');
+    expect(toString(['a', 'b', 'c'])).toBe('a,b,c');
+    expect(toString([symbol, symbol2, symbol3])).toBe(`${symbol.toString()},${symbol2.toString()},${symbol3.toString()}`);
+    expect(toString([1, 2, 3])).toBe('1,2,3');
+    expect(toString([NaN, NaN, NaN])).toBe('NaN,NaN,NaN');
+    expect(toString([null, null, null])).toBe(',,');
+    expect(toString([undefined, undefined, undefined])).toBe('undefined,undefined,undefined');
+    expect(toString([0.0, 0.5, 1.0])).toBe('0.0,0.5,1.0');
+    expect(toString([false, true, false])).toBe('false,true,false');
+    expect(toString([-Infinity, Infinity, Infinity])).toBe('-Infinity,Infinity,Infinity');
+  });
+
+  test('value is an object', () => {
+    expect(toString({})).toBe('[object Object]');
+    expect(toString({'a': 1, 'b': 2, 'c': 3})).toBe('[object Object]');
+  });
 });
