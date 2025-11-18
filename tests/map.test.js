b614fc390bdc0c880c1e07c82178c9ba35571c9b
AceHanded <vili.kononen@gmail.com>
2025-11-18 18:02:48 +0200

    Add unit tests for map.js and reduce.js (closes #6, closes #7)

--- tests/map.test.js
+++ tests/map.test.js
@@ -2,6 +2,75 @@
 
 import map from '../src/map.js';
 
-test('placeholder', () => {
-  expect(true).toBe(true);
+const addOne = (n) => +n + 1;
+
+describe('map', () => {
+  test('array and function are valid', () => {
+    expect(map([1, 2, 3], addOne)).toHaveLength(3).toEqual([2, 3, 4]);
+    expect(map(['1', '2', '3'], addOne)).toHaveLength(3).toEqual([2, 3, 4]);
+    expect(map([1.5, 2.5, 3.5], addOne)).toHaveLength(3).toEqual([2.5, 3.5, 4.5]);
+    expect(map([1, '2', 3.5], addOne)).toHaveLength(3).toEqual([2, 3, 4.5]);
+  });
+
+  test('function is null', () => {
+    expect(() => map([1, 2, 3], null)).toThrow(TypeError);
+  });
+
+  test('function is undefined', () => {
+    expect(() => map([1, 2, 3], undefined)).toThrow(TypeError);
+  });
+
+  test('function is NaN', () => {
+    expect(() => map([1, 2, 3], NaN)).toThrow(TypeError);
+  });
+
+  test('function throws error', () => {
+    expect(() => map([1, 2, 3], () => { throw new TypeError() })).toThrow(TypeError);
+  });
+
+  test('function returns truthy', () => {
+    expect(map([1, 2, 3], () => 1)).toHaveLength(3).toEqual([1, 1, 1]);
+    expect(map([1, 2, 3], () => 'Hello, World!')).toHaveLength(3).toEqual(['Hello, World!', 'Hello, World!', 'Hello, World!']);
+  });
+
+  test('function returns falsy', () => {
+    expect(map([1, 2, 3], () => 0)).toHaveLength(3).toEqual([0, 0, 0]);
+    expect(map([1, 2, 3], () => {})).toHaveLength(3).toEqual([undefined, undefined, undefined]);
+  });
+
+  test('array is a string', () => {
+    expect(map('123', addOne)).toHaveLength(3).toEqual([2, 3, 4]);
+    expect(map('123', (n) => n + n)).toHaveLength(3).toEqual(['11', '22', '33']);
+  });
+
+  test('array is empty', () => {
+    expect(map([], addOne)).toBeEmpty();
+  });
+
+  test('array contains single element', () => {
+    expect(map([3], addOne)).toHaveLength(1).toEqual([4]);
+    expect(map([1], addOne)).toHaveLength(1).toEqual([2]);
+  });
+
+  test.failing('array is non-iterable', () => {
+    expect(map(123, addOne)).toBe(123);
+    expect(map(true, addOne)).toBe(true);
+  });
+
+  test.failing('array is null', () => {
+    expect(map(null, addOne)).toBeNull();
+  });
+
+  test.failing('array is undefined', () => {
+    expect(map(undefined, addOne)).toBeUndefined();
+  });
+
+  test.failing('array is NaN', () => {
+    expect(map(NaN, addOne)).toBeNaN();
+  });
+
+  test('equal to built-in', () => {
+    expect(map([1, 2, 3], addOne)).toEqual([1, 2, 3].map(addOne));
+    expect(map([], addOne)).toEqual([].map(addOne));
+  });
 });
--- tests/reduce.test.js
+++ tests/reduce.test.js
@@ -2,6 +2,103 @@
 
 import reduce from '../src/reduce.js';
 
-test('placeholder', () => {
-  expect(true).toBe(true);
+describe('reduce', () => {
+  test('function is null', () => {
+    expect(() => reduce([1, 2, 3], null, 0)).toThrow(TypeError);
+  });
+
+  test('function is undefined', () => {
+    expect(() => reduce([1, 2, 3], undefined, 0)).toThrow(TypeError);
+  });
+
+  test('function is NaN', () => {
+    expect(() => reduce([1, 2, 3], NaN, 0)).toThrow(TypeError);
+  });
+
+  test('function throws error', () => {
+    expect(() => reduce([1, 2, 3], () => { throw new TypeError() }, 0)).toThrow(TypeError);
+  });
+
+  test('collection is an array', () => {
+    expect(reduce([1, 2, 3], (acc, n) => acc + n, 0)).toBe(6);
+    expect(reduce(['1', '2', '3'], (acc, n) => acc + n, '')).toBe('123');
+  });
+
+  test('collection is an object', () => {
+    expect(reduce({'a': 1, 'b': 2, 'c': 3}, (acc, n) => acc + n, 0)).toBe(6);
+    expect(reduce({'a': 1, 'b': 2, 'c': 3}, (acc, _, key) => acc + key,'')).toBe('abc');
+    expect(reduce({'a': 1, 'b': 2, 'c': 3}, (acc, val, key) => acc + val + key,'')).toBe('1a2b3c');
+  });
+
+  test('collection is a string', () => {
+    expect(reduce('123', (acc, n) => acc + n, '')).toBe('123');
+    expect(reduce('123', (acc, n) => acc + n, 0)).toBe('0123');
+  });
+
+  test('collection is empty', () => {
+    expect(reduce([], (acc, n) => acc + n, 0)).toBe(0);
+    expect(reduce({}, (acc, n) => acc + n, 0)).toBe(0);
+  });
+
+  test('collection contains single element', () => {
+    expect(reduce([1], (acc, n) => acc + n, 0)).toBe(1);
+    expect(reduce({'a': 1}, (acc, val, key) => acc + val + key,'')).toBe('1a');
+  });
+
+  test('collection is non-iterable', () => {
+    expect(reduce(123, (acc, n) => acc + n, 0)).toBe(0);
+    expect(reduce(true, (acc, n) => acc + n, 0)).toBe(0);
+  });
+
+  test('collection is null', () => {
+    expect(reduce(null, (acc, n) => acc + n, 0)).toBe(0);
+  });
+
+  test('collection is undefined', () => {
+    expect(reduce(undefined, (acc, n) => acc + n, 0)).toBe(0);
+  });
+
+  test('collection is NaN', () => {
+    expect(reduce(NaN, (acc, n) => acc + n, 0)).toBe(0);
+  });
+
+  test('accumulator is an array', () => {
+    expect(reduce([1, 2, 3], (acc, n) => [...acc, n], [])).toHaveLength(3).toEqual([1, 2, 3]);
+    expect(reduce([], (acc, n) => [...acc, n], [])).toBeEmpty();
+  });
+
+  test('accumulator is an object', () => {
+    expect(reduce({'a': 1, 'b': 2, 'c': 3}, (acc, val, key) => { acc[key] = val; return acc; }, {})).toEqual({'a': 1, 'b': 2, 'c': 3});
+    expect(reduce({}, (acc, n) => { acc[n] = n; return acc; }, {})).toBeEmpty();
+  });
+
+  test('accumulator is null', () => {
+    expect(reduce([1, 2, 3], (acc, n) => acc + n, null)).toBe(6);
+    expect(reduce([], (acc, n) => acc + n, null)).toBeNull();
+  });
+
+  test('accumulator is undefined', () => {
+    expect(reduce([1, 2, 3], (acc, n) => acc + n, undefined)).toBeNaN();
+    expect(reduce([], (acc, n) => acc + n, undefined)).toBeUndefined();
+  });
+
+  test('accumulator is NaN', () => {
+    expect(reduce([1, 2, 3], (acc, n) => acc + n, NaN)).toBeNaN();
+    expect(reduce([], (acc, n) => acc + n, NaN)).toBeNaN();
+  });
+
+  test('accumulator is omitted', () => {
+    expect(reduce([1, 2, 3], (acc, n) => acc + n)).toBe(6);
+    expect(reduce([], (acc, n) => acc + n)).toBeUndefined();
+  });
+
+  test.failing('function and accumulator are omitted', () => {
+    expect(() => reduce([1, 2, 3])).toThrow(TypeError);
+    expect(() => reduce([])).toThrow(TypeError);
+  });
+
+  test('equal to built-in', () => {
+    expect(reduce([1, 2, 3], (acc, n) => acc + n, 0)).toEqual([1, 2, 3].reduce((acc, n) => acc + n, 0));
+    expect(reduce([], (acc, n) => acc + n, 0)).toEqual([].reduce((acc, n) => acc + n, 0));
+  });
 });
