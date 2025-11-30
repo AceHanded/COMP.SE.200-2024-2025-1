49fa3bfb022fd5e99da7a3c08613b3033c513fad
Elias Poikonen <elias.poikonen@tuni.fi>
2025-11-30 22:56:52 +0200

    Add unit tests for divide.js and eq.js (closes #3, closes #4)

--- tests/divide.test.js
+++ tests/divide.test.js
@@ -2,6 +2,57 @@
 
 import divide from '../src/divide.js';
 
-test('placeholder', () => {
-  expect(true).toBe(true);
+describe('divide', () => {
+  test.failing('Division of two numbers', () => {
+    expect(divide(1,2)).toBe(1/2);
+    expect(divide(-4,-2)).toBe(-4/-2)
+    expect(divide(-2.4,2.4)).toBe(-2.4/2.4)
+  });
+
+  test('Division of number with itself', () => {
+    expect(divide(4,4)).toBe(1);
+    expect(divide(-2.52,-2.52)).toBe(1);
+  });
+
+  test.failing('Dividend is zero (0)', () => {
+    expect(divide(0,2)).toBe(0/2);
+  });
+
+  test.failing('Divisor is zero (0)', () => {
+    expect(divide(2,0)).toThrow(RangeError);
+  });
+
+  test.failing('Undefined parameters default to one (1)', () => {
+    expect(divide(2,)).toBe(2/1);
+    expect(divide(undefined,2)).toBe(1/2);
+    expect(divide()).toBe(1/1);
+  });
+
+  test.failing('Division includes infinity', () => {
+    expect(divide(Infinity,2)).toBe(Infinity);
+    expect(divide(2,Infinity)).toBe(2/Infinity);
+  });
+
+  test.failing('Division includes very large and very small numbers', () => {
+    expect(divide(Number.MAX_VALUE,2)).toBe(Number.MAX_VALUE/2);
+    expect(divide(2,Number.MIN_VALUE)).toBe(2/Number.MIN_VALUE);
+    expect(divide(Number.MIN_VALUE,Number.MAX_VALUE)).toBe(Number.MIN_VALUE/Number.MAX_VALUE);
+  });
+
+  test.failing('Division includes String', () => {
+    expect(divide("test",2)).toThrow(TypeError);
+    expect(divide(2,"test")).toThrow(TypeError);
+    expect(divide("2", "4")).toThrow(TypeError);
+  });
+
+  test.failing('Division includes bool', () => {
+    expect(divide(true,2)).toThrow(TypeError);
+    expect(divide(2,false)).toThrow(TypeError);
+  });
+
+  test.failing('Division includes object', () => {
+    expect(divide([2,1],2)).toThrow(TypeError);
+    expect(divide(2,{test: 2})).toThrow(TypeError);
+  });
+
 });
--- tests/eq.test.js
+++ tests/eq.test.js
@@ -2,6 +2,61 @@
 
 import eq from '../src/eq.js';
 
-test('placeholder', () => {
-  expect(true).toBe(true);
+const test_object = {'a': 1}
+const test_other = {'a': 1}
+
+describe('eq', () => {
+  test('Equal number values', () => {
+    expect(eq(1,1)).toBe(true);
+    expect(eq(NaN,NaN)).toBe(true);
+    expect(eq(2+3,5)).toBe(true);
+    expect(eq(2.5, 2.5000)).toBe(true);
+    expect(eq(test_object.a,test_other.a)).toBe(true);
+  });
+
+  test('Non-equal number values', () => {
+    expect(eq(1,2)).toBe(false);
+    expect(eq(-4,4)).toBe(false);
+    expect(eq(-Infinity, Infinity)).toBe(false);
+  });
+
+  test('String values', () => {
+    expect(eq("test","test")).toBe(true);
+    expect(eq("a","b")).toBe(false);
+  });
+
+  test('Boolean values', () => {
+    expect(eq(true,true)).toBe(true);
+    expect(eq(false,false)).toBe(true);
+    expect(eq(true,false)).toBe(false);
+  });
+
+  test('Object values', () => {
+    expect(eq(test_object,test_object)).toBe(true);
+    expect(eq(test_object,test_other)).toBe(false);
+    expect(eq([1,2],[1,2])).toBe(false);
+  });
+
+  test('Value and other are different types, equal', () => {
+    expect(eq(1,'1')).toBe(true);
+    expect(eq(false, 0)).toBe(true);
+    expect(eq('test',Object('test'))).toBe(true);
+    expect(eq(Object(2), String(2))).toBe(true);
+  });
+
+  test('Value and other are different types, non-equal', () => {
+    expect(eq(1,"test")).toBe(false);
+    expect(eq("true", true)).toBe(false);
+  });
+
+  test('Value or other is undefined', () => {
+    expect(eq(1,)).toBe(false);
+    expect(eq(undefined,"test")).toBe(false);
+    expect(eq()).toBe(true);
+  });
+
+  test('Null and undefined are equal', () => {
+    expect(eq(null, undefined)).toBe(true);
+  });
+
 });
